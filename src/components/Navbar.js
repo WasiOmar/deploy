46836732bc153350dfca6df19cbcfdb1f8cb6366
WasiOmar <wasiomar21@gmail.com>
2025-05-29 19:04:23 +0600

    messages added in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -63,6 +63,14 @@ const Navbar = ({ onLoginClick }) => {
                   Create Listing
                 </Link>
               )}
+              {user && (
+                <Link
+                  to="/messages"
+                  className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+                >
+                  Messages
+                </Link>
+              )}
               {/* Add the CMS link for admin users */}
               {user?.isAdmin && (
                 <Link
